fe6161782f9d7798cfa815527026c0f8b2b894de
drohit02 <drohit045@gmail.com>
2024-07-22 14:05:04 +0530

    Sign-In Api created

---
+++ sb-ecom/src/main/java/com/ecommerce/ecom/controllers/UserAuthController.java
@@ -0,0 +1,64 @@
+package com.ecommerce.ecom.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.ecom.jwt.JwtUtils;
+import com.ecommerce.ecom.jwt.LoginRequest;
+import com.ecommerce.ecom.jwt.LoginResponse;
+
+@RestController
+@RequestMapping("/api")
+public class UserAuthController {
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+		Authentication authentication;
+		try {
+			authentication = authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+		} catch (AuthenticationException exception) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("message", "Bad credentials");
+			map.put("status", false);
+			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+		}
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+		String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+
+		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+
+		LoginResponse response = new LoginResponse(jwtToken,userDetails.getUsername(), roles);
+
+		return ResponseEntity.ok(response);
+	}
+}
+
+
--- sb-ecom/src/main/java/com/ecommerce/ecom/jwt/LoginRequest.java
+++ sb-ecom/src/main/java/com/ecommerce/ecom/jwt/LoginRequest.java
@@ -1,5 +1,12 @@
 package com.ecommerce.ecom.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 	
 	private String username;
