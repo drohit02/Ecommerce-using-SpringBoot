0cea0671d3f0dd00f7b7fff7163948c5b8421273
drohit02 <drohit045@gmail.com>
2024-07-22 14:28:48 +0530

    jwt secret and expirations time set

--- sb-ecom/src/main/java/com/ecommerce/ecom/model/Product.java
+++ sb-ecom/src/main/java/com/ecommerce/ecom/model/Product.java
@@ -1,14 +1,11 @@
 package com.ecommerce.ecom.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
