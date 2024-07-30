a9e1a1a4fccd9f59e8cba0d6883cbe4f620270ab
drohit02 <drohit045@gmail.com>
2024-07-30 16:12:30 +0530

    Order,Payment,OrderItem created,DTOs created,Repository created

---
+++ sb-ecom/src/main/java/com/ecommerce/ecom/dto/OrderDTO.java
@@ -0,0 +1,24 @@
+package com.ecommerce.ecom.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+	private Long orderId;
+	private String email;
+	private LocalDate orderDate;
+	private double totalAmount;
+	private String orderStatus;
+	private PaymentDTO payment;
+	private List<OrderItemDTO> orederItems;
+	private AddressDTO address;
+
+}
